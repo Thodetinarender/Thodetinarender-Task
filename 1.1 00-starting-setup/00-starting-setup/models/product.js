8aa0423c5592b10300f0f746d01f5cdb29670729
THODETI NARENDER <thodetinarender51@gmail.com>
2025-01-21 21:02:12 +0530

    Dynamic Routes Part 1

--- 1.1 00-starting-setup/00-starting-setup/models/product.js
+++ 1.1 00-starting-setup/00-starting-setup/models/product.js
@@ -2,20 +2,29 @@
 const fs = require('fs');
 const path = require('path');
 
+ const p = path.join(
+       path.dirname(process.mainModule.filename),
+        'data',
+        'products.json'
+       );
+
+const getProductsFromFile =(cb)=>{
+       fs.readFile(p, (error, fileContent)=>{
+        if(error){
+            cb([]);
+        }else{
+          cb(JSON.parse(fileContent));
+        }
+     })
+
+}
+
 module.exports = class Product{
     constructor(t){
         this.title = t;
     }
     save() {
-      const p = path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'products.json'
-       );
-       fs.readFile(p, (error, fileContent)=>{
-        let products=[];
-        if(!error){
-            products = JSON.parse(fileContent);
-        }
+       getProductsFromFile(products=>{
         products.push(this);
         fs.writeFile(p, JSON.stringify(products),(error)=>{
             console.log(error);
@@ -24,16 +33,7 @@ module.exports = class Product{
    }
 
    static fetchAll(cb){
-     const p = path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'products.json'
-       );
-     fs.readFile(p, (error, fileContent)=>{
-        if(error){
-            cb([]);
-        }
-        cb(JSON.parse(fileContent));
-     })
+    getProductsFromFile(cb);
    }
 
 }
